6184c6a0e5902ed420b051a52e8a4728fdd8a022
Farman07-git <venomop21062007@gmail.com>
2026-07-04 15:57:52 +0500

    Add the file

---
+++ beginner_calc.py
@@ -0,0 +1,22 @@
+class Calculator :
+    def __init__(self, n):
+        self.n = n
+
+    def calc_sq(self):
+        print(f'The square of the number {self.n} is : {self.n**2}')
+
+    def calc_cube(self):
+        print(f'The cube of the number {self.n} is : {self.n**3}')
+
+    def calc_sq_root(self):
+        print(f'The square root of the number {self.n} is : {self.n**(1/2)}')
+
+
+a = Calculator(16)
+a.calc_sq()
+
+a = Calculator(12)
+a.calc_cube()
+
+a = Calculator(625)
+a.calc_sq_root()
